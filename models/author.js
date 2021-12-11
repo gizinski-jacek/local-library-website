3e1c1bf2621568a0804b735925f9a434667730af
mrjackmaster <jacektrg+github@gmail.com>
2021-12-11 19:17:22 +0100

    Arrow functions seem to not work here, reverting. Adding virtual for formatted lifespan

--- models/author.js
+++ models/author.js
@@ -10,7 +10,7 @@ const AuthorSchema = new Schema({
 });
 
 // Virtual for author's full name
-AuthorSchema.virtual('name').get(() => {
+AuthorSchema.virtual('name').get(function () {
 	let fullname = '';
 	if (this.first_name && this.family_name) {
 		fullname = this.family_name + ', ' + this.first_name;
@@ -22,7 +22,7 @@ AuthorSchema.virtual('name').get(() => {
 });
 
 // Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(() => {
+AuthorSchema.virtual('lifespan').get(function () {
 	let lifetime_string = '';
 	if (this.date_of_birth) {
 		lifetime_string = this.date_of_birth.getYear().toString();
@@ -34,8 +34,24 @@ AuthorSchema.virtual('lifespan').get(() => {
 	return lifetime_string;
 });
 
+// Virtual for author's lifespan formatted
+AuthorSchema.virtual('lifespan_formatted').get(function () {
+	if (!this.date_of_birth && !this.date_of_death) {
+		return 'no data';
+	}
+	let birth = '';
+	if (this.date_of_birth) {
+		birth = new Date(this.date_of_birth).toLocaleString('en-GB');
+	}
+	let death = '';
+	if (this.date_of_death) {
+		death = new Date(this.date_of_death).toLocaleString('en-GB');
+	}
+	return `${birth} - ${death}`;
+});
+
 // Virtual for author's URL
-AuthorSchema.virtual('url').get(() => {
+AuthorSchema.virtual('url').get(function () {
 	return '/catalog/author/' + this._id;
 });
 
