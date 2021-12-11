206b9726c63c68993e2a56f67a836943776aa5e1
mrjackmaster <jacektrg+github@gmail.com>
2021-12-11 19:16:13 +0100

    ADding catalog router

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const SomeModelSchema = new Schema({
 });
 
 const indexRouter = require('./routes/index');
+const catalogRouter = require('./routes/catalog');
 
 const app = express();
 
@@ -41,14 +42,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
 	next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
