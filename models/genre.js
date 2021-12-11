d9ca0183ae5ddcc53132f2f93649a0f1b87e1e33
mrjackmaster <jacektrg+github@gmail.com>
2021-12-11 19:17:40 +0100

    Arrow functions seem to not work here, reverting.

--- models/genre.js
+++ models/genre.js
@@ -7,7 +7,7 @@ const GenreSchema = new Schema({
 });
 
 // Virtual for genre's URL
-GenreSchema.virtual('url').get(() => {
+GenreSchema.virtual('url').get(function () {
 	return '/catalog/genre/' + this._id;
 });
 
