42cfc488125a95b35b43e98bbcc5ae89130829cb
mrjackmaster <jacektrg+github@gmail.com>
2021-12-15 16:58:52 +0100

    Adding controllers for delete GET and POST.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -182,13 +182,63 @@ exports.book_create_post = [
 ];
 
 // Display book delete form on GET.
-exports.book_delete_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book delete GET');
+exports.book_delete_get = (req, res, next) => {
+	async.parallel(
+		{
+			book: (cb) => {
+				Book.findById(req.params.id).populate('author').exec(cb);
+			},
+			book_bookinstances: (cb) => {
+				BookInstance.find({ book: req.params.id }).exec(cb);
+			},
+		},
+		(err, results) => {
+			if (err) {
+				return next(err);
+			}
+			if (results.book == null) {
+				res.redirect('/catalog/books');
+			}
+			res.render('book_delete', {
+				title: 'Delete Book',
+				book: results.book,
+				book_bookinstances: results.book_bookinstances,
+			});
+		}
+	);
 };
 
 // Handle book delete on POST.
-exports.book_delete_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book delete POST');
+exports.book_delete_post = (req, res, next) => {
+	async.parallel(
+		{
+			book: (cb) => {
+				Book.findById(req.params.id).populate('author').exec(cb);
+			},
+			book_bookinstances: (cb) => {
+				BookInstance.find({ book: req.params.id }).exec(cb);
+			},
+		},
+		(err, results) => {
+			if (err) {
+				return next(err);
+			}
+			if (results.book_bookinstances.length > 0) {
+				res.render('book_delete', {
+					title: 'Delete Book',
+					book: results.book,
+					book_bookinstances: results.book_bookinstances,
+				});
+			} else {
+				Book.findByIdAndDelete(req.body.bookid, (err) => {
+					if (err) {
+						return next(err);
+					}
+					res.redirect('/catalog/books');
+				});
+			}
+		}
+	);
 };
 
 // Display book update form on GET.
